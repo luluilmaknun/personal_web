23bdcd38fcf16cdb4ca96b74b5ccd0a3999365db
Lulu <lulu.ilmaknun.q@gmail.com>
2019-03-04 05:06:43 -0800

    done simple add event

--- ToDo/views.py
+++ ToDo/views.py
@@ -1,6 +1,6 @@
 from .models import *
 from .forms import *
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import datetime, calendar
 import json
