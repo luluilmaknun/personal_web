5d9082b41ce09b46512a4c4c246842acf47e025a
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2019-10-08 11:16:52 +0700

    create jumbotron parallax

---
+++ new_website/static/new_website/js/parallax.js
@@ -0,0 +1,18 @@
+$(function() {
+    var parallax_layers = 6;
+
+    $(window).scroll(function() {
+        var count = 0;
+
+        $('div[class*=parallax__layer__]').each(function() {
+            var x = (parallax_layers - count) / 5;
+            var transform = 'translateY(-' + ($(window).scrollTop() * count / parallax_layers) + 'px)';
+            $(this).css('transform', transform);
+
+            if(count == 6) {
+                $('.parallax__layer_after').css('transform', transform);
+            }
+            count++;
+        });
+    })
+})
--- new_website/views.py
+++ new_website/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    print("masuk")
     return render(request, 'new_website/index.html', {'title': 'Random Lulz Things'})
